86c6e6461ecfd1b42ed1e3070d144801eb37b226
sbein <sbein@umich.edu>
2019-09-09 17:40:57 -0400

    complete lab 0

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import random
+
+print('Scott')
+num1 = random.randint(0, 100)
+num2 = random.randint(0, 100)
+print(num1)
+print(num2)
+print("Sum =", num1 + num2)
+print("Average =", num1 + num2 / 2)
